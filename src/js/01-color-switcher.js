8cacd79a79ce80ae3e6941813a2423c314255b8e
noname068218 <128069790+noname068218@users.noreply.github.com>
2023-09-17 12:34:03 +0200

    Опн

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,4 +1,5 @@
 import 'particles.js';
+import 'stats.js';
 
 const btnStartColor = document.querySelector('button[data-start]');
 const btnStopColor = document.querySelector('button[data-stop]');
@@ -145,23 +146,3 @@ particlesJS('particles-js', {
   },
   retina_detect: true,
 });
-
-/* ---- stats.js config ---- */
-
-var count_particles, stats, update;
-stats = new Stats();
-stats.setMode(0);
-stats.domElement.style.position = 'absolute';
-stats.domElement.style.left = '0px';
-stats.domElement.style.top = '0px';
-document.body.appendChild(stats.domElement);
-count_particles = document.querySelector('.js-count-particles');
-update = function () {
-  stats.begin();
-  stats.end();
-  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-  }
-  requestAnimationFrame(update);
-};
-requestAnimationFrame(update);
